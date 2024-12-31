a14b39512df2f3bc592f36b36f8725ed8e25b655
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-31 14:25:55 +0530

    change button

--- src/components/about/Counter.jsx
+++ src/components/about/Counter.jsx
@@ -17,14 +17,14 @@ function Counter() {
             <h2 className="text-center mb-4 Map-heading">Counter UseState</h2>
             <div className="d-flex justify-content-center align-items-center">
                 <button
-                    className="btn btn-danger mx-2"
+                    className="p-3 border-gray text-black rounded-2 "
                     onClick={decrement}
                     disabled={count <= 0} 
                 >
                     Decrement
                 </button>
                 <h3 className="mx-4">{count}</h3>
-                <button className="btn btn-success mx-2" onClick={increment}>
+                <button className="p-3 border-gray text-black rounded-2 " onClick={increment}>
                     Increment
                 </button>
             </div>
