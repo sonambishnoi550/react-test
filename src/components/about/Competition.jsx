b9e51441b71f3a37e2fe3e5c822832b12e2cc3ad
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-31 13:44:26 +0530

    change logo

--- src/components/about/Competition.jsx
+++ src/components/about/Competition.jsx
@@ -3,30 +3,30 @@ import { COMPETITION_ITEMS } from '../../utils/helper'
 
 const Competition = () => {
     return (
-        <div className='min-vh-100 py-5 d-flex competition-section justify-content-center align-items-center dark-black'>
+        <div className='min-vh-100 py-5 d-flex justify-content-center align-items-center dark-black'>
             <div className='competition-container row row-gap-4'>
                 <div className='col-lg-6 col-12'>
                     <h1 className="text-white main-heading">Competition</h1>
-                    <p className="m-0 text-white competition-text common-description">
+                    <p className="mb-2 text-white competition-text">
                         Given our best in class offering, we need to cut through by opting for penetration pricing.
                     </p>
-                    <p className="m-0 mt-2 text-white competition-text common-description">
+                    <p className="mb-2 text-white competition-text product-pera">
                         This will allow us to disrupt an established market by introducing our product and service at a lower price to entice new customers to purchase or subscribe. This strategy will help us capture the attention of this very large audience in need of a good solution.
                     </p>
-                    <p className="m-0 mt-2 text-white competition-text common-description">
+                    <p className="mb-2 text-white competition-text product-pera">
                         The current return and refund process centres around either traditional advice or software. By combining both together for an aggressively affordable fee, we think we can capture much of the demand for those people that feel competent in managing their own refunds / returns.
                     </p>
-                    <p className="m-0 mt-2 text-white competition-text common-description">
+                    <p className="m-0 text-white competition-text product-pera">
                         As well as being the cheapest in market, our product offers everything a sole trader needs in one place, whilst pricing innovation of zero commission rather than a commission, will drive customers that are particularly price sensitive.
                     </p>
                 </div>
-                <div className="col-12 col-lg-6 table-bg d-flex border-radius-xl align-items-center justify-content-center sm-py-xl overflow-hidden">
+                <div className="col-12 col-lg-6 table-bg d-flex border-radius-sm align-items-center justify-content-center sm-py-xl overflow-hidden">
                     <div className="md-d-flex align-items-center justify-content-center w-100">
                         <div className="table-responsive">
                             <table className="custom-table-bg py-5">
                                 <thead>
                                     <tr>
-                                        <th class=" d-sm-table-cell"></th>
+                                        <th></th>
                                         <th className="x-radius-top text-black text-center bg-white table-heading">Rift</th>
                                         <th className="x-radius-top text-black text-center bg-white table-heading">Taxscout</th>
                                         <th className="x-radius-top text-black text-center bg-white table-heading">Coconut</th>
