da4d57514cbf967f14f71fb475f19faf408548cc
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-31 09:26:48 +0530

    same

--- src/components/about/Competition.jsx
+++ src/components/about/Competition.jsx
@@ -1,91 +1,56 @@
 import React from 'react'
+import { COMPETITION_LIST } from '../../utils/helper'
 
-const Compition = () => {
+const Competition = () => {
     return (
-        <div className="dark-black min-vh-100 d-flex align-items-center justify-content-center pb-5">
-            <div className="container">
-                <div className="row gap-3 align-items-center justify-content-center">
-                    <div className="col-lg-5 px-4">
-                        <h1 className="main-heading text-white pt-3">compition</h1>
-                        <p className="product-pera text-white mb-2 max-w-481">
-                            Given our best in class offering, we need to cut through by opting for penetration pricing.
-                        </p>
-                        <p className="product-pera text-white mb-2 max-w-481">
-                            This will allow us to disrupt an established market by introducing our product and service at a lower price to entice new customers to purchase or subscribe. This strategy will help us capture the attention of this very large audience in need of a good solution.
-                        </p>
-                        <p className="product-pera text-white mb-2 max-w-481">
-                            The current return and refund process centres around either traditional advice or software. By combining both together for an aggressively affordable fee, we think we can capture much of the demand for those people that feel competent in managing their own refunds / returns.
-                        </p>
-                        <p className="product-pera text-white mb-2 pb-5 max-w-481">
-                            As well as being the cheapest in market, our product offers everything a sole trader needs in one place, whilst pricing innovation of zero commission rather than a commission, will drive customers that are particularly price sensitive.
-                        </p>
-                    </div>
-                    <div className="col-lg-6 sky py-5 rounded-4 ">
-                        <div className="overflow-x">
-                            <table >
-                                <tr className='border-bottom border-primary border-black justify-content-between gap-5'>
-                                    <th className='table-padding '></th>
-                                    <th className='table-padding text-center table-x-padding bg-white br-top-table mx-2 table-heading'>Rift</th>
-                                    <th className='table-padding text-center px-4  bg-white br-top-table table-heading'>Taxscouts</th>
-                                    <th className='table-padding text-center table-x-padding bg-white br-top-table table-heading'>Coconut</th>
-                                    <th className='table-padding text-center table-x-padding light-green-color br-top-table table-heading'>Pie</th>
-                                </tr>
-                                <tr className='border-bottom border-primary border-black table-padding'>
-                                    <th className='table-padding text-white table-text'>  Mobile App</th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding light-green-color'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                </tr>
-                                <tr className='border-bottom border-primary border-black table-padding'>
-                                    <th className='table-padding text-white table-text'>  Invoicing</th>
-                                    <th className='table-padding text-center table-x-padding bg-white '><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding light-green-color'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                </tr>
-                                <tr className='border-bottom border-primary border-black table-padding'>
-                                    <th className='table-padding text-white table-text'>  Bookkeeping</th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding light-green-color'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                </tr>
-                                <tr className='border-bottom border-primary border-black table-padding '>
-                                    <th className='table-padding text-white table-text'>Accountancy support</th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/wrong.svg" alt="wrong" /></th>
-                                    <th className='table-padding text-center table-x-padding light-green-color'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                </tr>
-                                <tr className='border-bottom border-primary border-black table-padding'>
-                                    <th className='table-padding text-white table-text'>Tax Filing</th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding bg-white'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                    <th className='table-padding text-center table-x-padding light-green-color'><img src="./assets/images/svg/right.svg" alt="right" /></th>
-                                </tr>
-                                <tr className='  table-padding'>
-                                    <th className='table-padding text-white table-text'>Fee</th>
-                                    <th className='table-padding table-data max-w-50 px-4 bg-white br-bottom-table'>Est: £300
-                                        per year</th>
-                                    <th className='table-padding table-data max-w-50 px-4 bg-white br-bottom-table'>£300
-                                        per year</th>
-                                    <th className='table-padding table-data max-w-50 px-4 bg-white br-bottom-table'>£9.00 p/m
-                                        £108 per year</th>
-                                    <th className='table-padding table-data max-w-90 light-green-color br-bottom-table'>£6.99 or £9.99p/m
-                                        £59.99 review fee
-                                        £143.87 per year</th>
-                                </tr>
+        <div className='min-vh-100 py-5 d-flex competition-section justify-content-center align-items-center dark-black'>
+            <div className='competition-container row row-gap-4'>
+                <div className='col-lg-6 col-12'>
+                    <h1 className="text-white common-heading">Competition</h1>
+                    <p className="m-0 text-white competition-text common-description">
+                        Given our best in class offering, we need to cut through by opting for penetration pricing.
+                    </p>
+                    <p className="m-0 mt-2 text-white competition-text common-description">
+                        This will allow us to disrupt an established market by introducing our product and service at a lower price to entice new customers to purchase or subscribe. This strategy will help us capture the attention of this very large audience in need of a good solution.
+                    </p>
+                    <p className="m-0 mt-2 text-white competition-text common-description">
+                        The current return and refund process centres around either traditional advice or software. By combining both together for an aggressively affordable fee, we think we can capture much of the demand for those people that feel competent in managing their own refunds / returns.
+                    </p>
+                    <p className="m-0 mt-2 text-white competition-text common-description">
+                        As well as being the cheapest in market, our product offers everything a sole trader needs in one place, whilst pricing innovation of zero commission rather than a commission, will drive customers that are particularly price sensitive.
+                    </p>
+                </div>
+                <div className="col-12 col-lg-6 table-bg d-flex border-radius-xl align-items-center justify-content-center sm-py-xl overflow-hidden">
+                    <div className="d-flex align-items-center justify-content-center w-100">
+                        <div className="table-responsive">
+                            <table className="custom-table-bg py-5">
+                                <thead>
+                                    <tr>
+                                        <th class=" d-sm-table-cell"></th>
+                                        <th className="x-radius-top text-center bg-white table-heading">Rift</th>
+                                        <th className="x-radius-top text-center bg-white table-heading">Taxscout</th>
+                                        <th className="x-radius-top text-center bg-white table-heading">Coconut</th>
+                                        <th className="x-radius-top text-center bg-dark-aqua table-heading">Pie</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {COMPETITION_LIST.map((item, i) => (
+                                        <tr key={i}>
+                                            <td className={`text-white fw-normal Competition-td bg-transparent border-b-none ${i === 3 ? 'max-w-sm' : ''}`}>{item.brand}</td>
+                                            <td className={`bg-white Competition-td border-b-none text-center fee-heading ${i === 5 ? 'x-radius-bottom ' : ''} `}>{item.rift}</td>
+                                            <td className={`bg-white Competition-td border-b-none text-center fee-heading ${i === 5 ? 'x-radius-bottom ' : ''} `}>{item.taxscout}</td>
+                                            <td className={`bg-white Competition-td border-b-none text-center fee-heading ${i === 5 ? 'x-radius-bottom ' : ''}`}>{item.coconut}</td>
+                                            <td className={`bg-dark-aqua Competition-td border-b-none text-center fee-heading ${i === 5 ? 'x-radius-bottom px-sm' : ''}`}>{item.pie}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     )
 }
 
-export default Compition
+export default Competition
---
+++ src/utils/helper.js
@@ -0,0 +1,104 @@
+export const COMPETITION_LIST = [
+    {
+        brand: "Mobile App",
+        rift: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        taxscout: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        coconut: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        pie: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+    },
+    {
+        brand: "Invoicing",
+        rift: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        taxscout: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        coconut: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        pie: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+    },
+    {
+        brand: "Bookkeeping",
+        rift: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        taxscout: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        coconut: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        pie: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+    },
+    {
+        brand: "Accountancy support",
+        rift: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        taxscout: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        coconut: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14903 14.8391L4.09607 13.7862L8.3079 9.57436L4.09607 5.36253L5.14903 4.30957L9.36086 8.5214L13.5727 4.30957L14.6256 5.36253L10.4138 9.57436L14.6256 13.7862L13.5727 14.8391L9.36086 10.6273L5.14903 14.8391Z" fill="black" />
+        </svg>
+        ),
+        pie: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+    },
+    {
+        brand: "Tax Filing",
+        rift: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        taxscout: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        coconut: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+        pie: (<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.8984 6.13589L7.30493 13.7294L3.82458 10.249L4.71682 9.35679L7.30493 11.9386L14.0062 5.24365L14.8984 6.13589Z" fill="black" />
+        </svg>
+        ),
+    },
+    {
+        brand: "Fee",
+        rift: "Est: £300 per year",
+        taxscout: "£300 per year",
+        coconut: "£9.00 p/m £108 per year",
+        pie: "£6.99 or £9.99p/m £59.99 review fee £143.87 per year",
+    },
+];
